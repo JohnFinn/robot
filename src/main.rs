032acb1c40cee496937196b5e5aa23b1b63854d1
jouny <dz4tune@gmail.com>
2019-01-13 19:21:25 +0300

    initial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+extern crate ggez;
+
+use ggez::{Context, ContextBuilder, GameError, conf, event, GameResult};
+use ggez::graphics::{Point2,Vector2};
+use ggez::event::EventHandler;
+use ggez::graphics::{self,Mesh,DrawMode};
+
+mod world;
+use self::world::*;
+
+impl EventHandler for World {
+    fn update(&mut self, context: &mut Context) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, context: &mut Context) -> GameResult<()> {
+        graphics::clear(context);
+        graphics::set_background_color(context, (255, 255, 255, 0).into());
+        graphics::set_color(context, graphics::BLACK)?;
+        for round in self.rounds.iter() {
+            let center = to_screen_coordinates(context, &round.center);
+            let radius = to_screen_distanse(context, round.radius);
+            graphics::circle(context, DrawMode::Fill, center, radius, 0.1)?;
+        }
+        graphics::present(context);
+        Ok(())
+    }
+}
+
+
+fn to_screen_coordinates(context: &Context, point: &Vector2) -> Point2 {
+    let x = (1.0 + point.x) * context.conf.window_mode.width as f32 / 2.0;
+    let y = (1.0 - point.y) * context.conf.window_mode.height as f32 / 2.0;
+    Point2::new(x, y)
+}
+
+fn to_screen_distanse(context: &Context, distance: f32) -> f32 {
+    distance * context.conf.window_mode.width as f32
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut cb = ContextBuilder::new("robot", "jouny")
+        .window_setup(conf::WindowSetup::default().title("robot"))
+        .window_mode(conf::WindowMode::default().dimensions(1000, 1000));
+    let context = &mut cb.build().unwrap();
+    let rounds = vec![
+        Round::new(1.0, 1.0, 0.1),
+        Round::new(-1.0, 0.0, 0.05),
+        Round::new(1.0, -1.0, 0.025),
+    ];
+    let mut state = World::new(rounds);
+    if let Err(e) = event::run(context, &mut state) {
+        println!("Error encountered running game: {}", e);
+    } else {
+        println!("Game exited cleanly.");
+    }
 }
---
+++ src/world.rs
@@ -0,0 +1,55 @@
+use ggez::graphics::Vector2;
+
+pub struct Robot {
+    pub position: Vector2,
+    pub speed: Vector2
+}
+
+impl Robot {
+    pub fn push(&mut self, force: &Vector2, time: f32){
+        self.position += self.speed * time;
+        self.speed += force * time;
+    }
+}
+
+pub struct Round{
+    pub center: Vector2,
+    pub radius: f32,
+}
+
+impl Round {
+    pub fn new(x: f32, y: f32, r:f32) -> Round {
+        Round{center:Vector2::new(x,y), radius: r}
+    }
+}
+
+pub struct World {
+    pub rounds: Vec<Round>,
+    pub robot: Robot
+}
+
+impl World {
+    pub fn new(rounds: Vec<Round>) -> World {
+        World{
+            rounds: rounds,
+            robot: Robot{
+                position:Vector2::new(-1.0,-1.0),
+                speed: Vector2::new(0.0, 0.0)
+            }
+        }
+    }
+
+    pub fn push_robot(&mut self, force: &Vector2, time: f32){
+        self.robot.push(force, time);
+    }
+
+    pub fn check_collisions(&self) -> bool {
+        for round in self.rounds.iter() {
+            let r = round.center - self.robot.position;
+            if r.x * r.x + r.y * r.y <= round.radius * round.radius {
+                return false;
+            }
+        }
+        return true;
+    }
+}
